5f4c7e84104f0f3daa8e56354341e097cc3c3a02
marco-mart <martinez.marco.1027@gmail.com>
2023-02-03 16:34:41 -0700

    Created new branch initializing_in_memory_data

--- Martinez-Chacon-Summative-Project-1/src/main/java/com/company/MartinezChaconSummativeProject1/controller/Magic8BallController.java
+++ Martinez-Chacon-Summative-Project-1/src/main/java/com/company/MartinezChaconSummativeProject1/controller/Magic8BallController.java
@@ -1,8 +1,25 @@
 package com.company.MartinezChaconSummativeProject1.controller;
 
+import com.company.MartinezChaconSummativeProject1.models.Magic8Ball;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Magic8BallController
 {
+
+    private List<Magic8Ball> magic8Balls;
+
+    private static int idCounter = 1;
+    public Magic8BallController()
+    {
+        magic8Balls = new ArrayList<Magic8Ball>();
+
+        Magic8Ball ball1 = new Magic8Ball();
+        ball1.setId(idCounter++);
+        ball1.setQuestion("");
+        ball1.setAnswer("Maybe");
+    }
 }
