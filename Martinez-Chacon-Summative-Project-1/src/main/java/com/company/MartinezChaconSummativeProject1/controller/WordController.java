46dbf03732877ccc002a8cc8663f7aff16aca861
marco-mart <martinez.marco.1027@gmail.com>
2023-02-03 18:15:10 -0700

    Created a test for Word Controller's GET "/word" endpoint response status, and implemented the endpoint. It seems to work, may require further testing.

--- Martinez-Chacon-Summative-Project-1/src/main/java/com/company/MartinezChaconSummativeProject1/controller/WordController.java
+++ Martinez-Chacon-Summative-Project-1/src/main/java/com/company/MartinezChaconSummativeProject1/controller/WordController.java
@@ -1,6 +1,9 @@
 package com.company.MartinezChaconSummativeProject1.controller;
 
 import com.company.MartinezChaconSummativeProject1.models.Definition;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -26,11 +29,29 @@ public class WordController
         definition1.setWord("Serendipity");
         definition1.setDefinition("The occurrence and development of events by chance in a happy or beneficial way.");
 
+        definitions.add(definition1);
+
         Definition definition2 = new Definition();
         definition2.setId(idCounter++);
         definition2.setWord("Stipulation");
         definition2.setDefinition("A condition or requirement that is specified or demanded as part of an agreement.");
 
+        definitions.add(definition2);
+    }
+
+    /**
+     * Endpoint "/word" which will return a random definition for a word
+     * @return a random definition
+     */
+    @GetMapping(value = "/word")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Definition getRandomDefinitionOfWord()
+    {
+        // generate random index for in-memory data
+        int range = definitions.size();
+        int randomIndex = (int) (Math.random() * range);
+
+        return definitions.get(randomIndex);
     }
 
 
