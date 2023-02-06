0b1d914641f9ad9d68ec3864a41d88d1fc92737d
marco-mart <martinez.marco.1027@gmail.com>
2023-02-06 13:32:50 -0700

    Implemented Quotes API and MockMvc tests for the Quote controller.

--- Martinez-Chacon-Summative-Project-1/src/test/java/com/company/MartinezChaconSummativeProject1/QuoteControllerTests.java
+++ Martinez-Chacon-Summative-Project-1/src/test/java/com/company/MartinezChaconSummativeProject1/QuoteControllerTests.java
@@ -4,14 +4,39 @@
 package com.company.MartinezChaconSummativeProject1;
 
 import com.company.MartinezChaconSummativeProject1.controller.QuoteController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(QuoteController.class)
 public class QuoteControllerTests
 {
+    @Autowired
+    private MockMvc mockMvc;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Tests response status of Quote Controller endpoint
+     * "/quote" should be "200 OK"
+     * @throws Exception
+     */
+    @Test
+    public void shouldReturnHTTPResponseStatus200Ok() throws Exception
+    {
+        // Arrange : none
+
+        // Act
+        mockMvc.perform(get("/quote"))
+                .andExpect(status().isOk());
+    }
 
 }
